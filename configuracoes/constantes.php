676507e018bd14125beb6cb4fd1a1c28fbb382c8
Danilo Araújo Silva <dan.garou@gmail.com>
2012-11-22 17:17:32 -0200

    Novos arquivos

---
+++ configuracoes/constantes.php
@@ -0,0 +1,2 @@
+<?php
+define("DIRETORIO_PAGAMENTOS","/tipos/");
--- index.php
+++ index.php
@@ -1,1 +1,29 @@
 <?php
+include(DIRETORIO_PAGAMENTOS);
+?>
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="css/index.css" />
+  </head>
+  <body>
+    <h1>Formas de pagamento</h1>
+    <ul>
+      <li>
+        <a href="tipos/boleto.php">
+          Boleto bancário
+        </a>
+      </li>
+      <li>
+        <a href="tipos/mastercard.php">
+          Mastercard
+        </a>
+      </li>
+      <li>
+        Visa
+      </li>
+      <li>
+        PagSeguro
+      </li>
+    </ul>
+  </body>
+</html>
